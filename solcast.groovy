16c8fa1faea8f15bf88c5a358a278c732d5dd8e8
youzer-name <57046429+youzer-name@users.noreply.github.com>
2024-05-17 11:29:26 -0400

    Update solcast.groovy
    Fixed error with 72 hour high estimate value. Added html tile option. Set version to 1.0.0.

--- solcast.groovy
+++ solcast.groovy
@@ -31,14 +31,16 @@ metadata {
         attribute "72 Hour Estimate - High", "number"
         attribute "72 Hour Estimate - Low", "number"
         attribute "lastUpdate", "string"
+        attribute "html24hour", "string"
     }
     preferences {
         input name: "logEnable", type: "bool", title: "Enable Info logging", defaultValue: true, description: ""
-        input name: "debugLog", type: "bool", title: "Enable Debug logging", defaultValue: true, description: ""
+        input name: "debugLog", type: "bool", title: "Enable Debug logging", defaultValue: false, description: ""
+        input name: "htmlTile", type: "bool", title: "Create Dashboard Tile?", defaultValue: false, description: ""
         input name: "api_key", type: "string", title: "API Key", required: true
         input name: "resource_id_a", type: "string", title: "Site Resource ID_a", required: true
         input name: "resource_id_b", type: "string", title: "Site Resource ID_b", required: true
-        input("refresh_interval", "enum", title: "How often to refresh the battery data", options: [
+        input("refresh_interval", "enum", title: "How often to refresh the forecast data", options: [
             0: "Do NOT update",
             30: "30 minutes",
             1: "1 Hour",
@@ -46,12 +48,12 @@ metadata {
             8: "8 Hours",
             12: "12 Hours",
             24: "Daily",
-        ], required: true, defaultValue: "3")
+        ], required: true, defaultValue: "0")
     }
 }
 
 def version() {
-    return "1.0.6"
+    return "1.0.0"
 }
 
 def installed() {
@@ -84,6 +86,7 @@ def updated() {
 
 import groovy.json.JsonOutput;
 def refresh() {
+     
     outputTZ = TimeZone.getTimeZone('UTC')
 
     def next1 = 0;
@@ -211,21 +214,31 @@ def refresh() {
     
     def est_24hour_high = (next24High_a + next24High_b).setScale(2, BigDecimal.ROUND_HALF_UP)
     def est_48hour_high = (next48High_a + next48High_b).setScale(2, BigDecimal.ROUND_HALF_UP)
-    def est_72hour_high = (next72Low_a + next72Low_b).setScale(2, BigDecimal.ROUND_HALF_UP)
+    def est_72hour_high = (next72High_a + next72High_b).setScale(2, BigDecimal.ROUND_HALF_UP)
     
     def est_24hour_low = (next24Low_a + next24Low_b).setScale(2, BigDecimal.ROUND_HALF_UP)
     def est_48hour_low = (next48Low_a + next48Low_b).setScale(2, BigDecimal.ROUND_HALF_UP)
     def est_72hour_low = (next72Low_a + next72Low_b).setScale(2, BigDecimal.ROUND_HALF_UP)
     
+//round values
+    def rnd_peak24_a = peak24_a.pv_estimate.setScale(2, BigDecimal.ROUND_HALF_UP)
+    def rnd_peak24_b = peak24_b.pv_estimate.setScale(2, BigDecimal.ROUND_HALF_UP)
+    def rnd_peak48_a = peak48_a.pv_estimate.setScale(2, BigDecimal.ROUND_HALF_UP)
+    def rnd_peak48_b = peak48_b.pv_estimate.setScale(2, BigDecimal.ROUND_HALF_UP)
+    def rnd_peak72_a = peak72_a.pv_estimate.setScale(2, BigDecimal.ROUND_HALF_UP)
+    def rnd_peak72_b = peak72_b.pv_estimate.setScale(2, BigDecimal.ROUND_HALF_UP)
+    def rnd_next1_a = (next1_a*1000).setScale(2, BigDecimal.ROUND_HALF_UP)
+    def rnd_next1_b = (next1_a*1000).setScale(2, BigDecimal.ROUND_HALF_UP)
+    
 //send events
-    sendEvent(name: "24 Hour Peak Production_a", value: peak24_a.pv_estimate.setScale(2, BigDecimal.ROUND_HALF_UP))
-    sendEvent(name: "24 Hour Peak Production_b", value: peak24_b.pv_estimate.setScale(2, BigDecimal.ROUND_HALF_UP))
-    sendEvent(name: "48 Hour Peak Production_a", value: peak48_a.pv_estimate.setScale(2, BigDecimal.ROUND_HALF_UP))
-    sendEvent(name: "48 Hour Peak Production_b", value: peak48_b.pv_estimate.setScale(2, BigDecimal.ROUND_HALF_UP))
-    sendEvent(name: "72 Hour Peak Production_a", value: peak72_a.pv_estimate.setScale(2, BigDecimal.ROUND_HALF_UP))
-    sendEvent(name: "72 Hour Peak Production_b", value: peak72_b.pv_estimate.setScale(2, BigDecimal.ROUND_HALF_UP))
-    sendEvent(name: "1 Hour Estimate_a", value: (next1_a*1000).setScale(2, BigDecimal.ROUND_HALF_UP))
-    sendEvent(name: "1 Hour Estimate_b", value: (next1_a*1000).setScale(2, BigDecimal.ROUND_HALF_UP))
+    sendEvent(name: "24 Hour Peak Production_a", value: rnd_peak24_a)
+    sendEvent(name: "24 Hour Peak Production_b", value: rnd_peak24_b)
+    sendEvent(name: "48 Hour Peak Production_a", value: rnd_peak48_a)
+    sendEvent(name: "48 Hour Peak Production_b", value: rnd_peak48_b)
+    sendEvent(name: "72 Hour Peak Production_a", value: rnd_peak72_a)
+    sendEvent(name: "72 Hour Peak Production_b", value: rnd_peak72_b)
+    sendEvent(name: "1 Hour Estimate_a", value: rnd_next1_a)
+    sendEvent(name: "1 Hour Estimate_b", value: rnd_next1_b)
     sendEvent(name: "1 Hour Estimate", value: est_1hour)
     sendEvent(name: "one_hour_estimate", value: est_1hour)
     sendEvent(name: "24 Hour Estimate", value: est_24hour)
@@ -245,5 +258,20 @@ def refresh() {
 
 //log message for totals
     if(logEnable) log.info  "{ \"next1\": " + est_1hour + ", \"next24\": " +  est_24hour + ", \"next24High\": " +  est_24hour_high + ", \"next24Low\": " + est_24hour_low  + ", \"next48\": " + est_48hour + ", \"next48High\": " + est_48hour_high + ", \"next48Low\": " + est_48hour_low + ", \"next72\": " + est_72hour + ", \"next72High\": " + est_72hour_high + ", \"next72Low\": " +  est_72hour_low + "}";    
+    
+// Create Dashboard tile
+    if(htmlTile) {
+        
+        html24hour ="<div style='line-height:1.0; font-size:0.75em;'><br>Total Est: ${est_24hour} kWh<br></div>"
+	    html24hour +="<div style='line-height:50%;'><br></div>"
+	    html24hour +="<div style='line-height:1.0; font-size:0.75em;'><br>Peak A: ${rnd_peak24_a} kW<br></div>"
+	    html24hour +="<div style='line-height:1.0; font-size:0.75em;'><br>Peak B: ${rnd_peak24_b} kW<br></div>"
+        html24hour +="<div style='line-height:50%;'><br></div>"
+        html24hour +="<div style='line-height:1.0; font-size:0.6em;'><br>Updated: ${now}</div>"
+    
+        sendEvent(name: "html24hour", value: html24hour)
+        
+        if(debugLog) {log.debug "html24hour contains ${html24hour}"}
+	}
     
 }
